90347f872058cddf68739bf895ea2425bbf9132f
aaquaa <113173827+aaquaa@users.noreply.github.com>
2023-12-29 15:06:52 +1100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -161,25 +161,8 @@ export default function Home() {
         </form>
       </main>
       <footer>
-        <div className="mt-8 flex h-24 flex-col items-center justify-center gap-4">
-          <Image
-            alt="Red Hawk Robotics"
-            src="/RedHawkRobotics-logo.svg"
-            width={400}
-            height={200}
-          />
-          <a
-            href="https://vercel.com/?utm_source=iraiders&utm_campaign=oss"
-            target="_blank"
-          >
-            <Image
-              alt="Powered by Vercel"
-              src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
-              width={200}
-              height={200}
-            ></Image>
-          </a>
-        </div>
+this is cool
+        
       </footer>
     </div>
   )
