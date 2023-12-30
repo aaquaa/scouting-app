d404a3fa08f507f17763dcc1a20ac9bcc5234c38
aaquaa <113173827+aaquaa@users.noreply.github.com>
2023-12-30 13:03:52 +1100

    Update index.tsx
    test, ishould find degault var next

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import {
   uploadConfig,
   useQRScoutState,
 } from '../components/store/store'
+import { useTheme } from 'next-themes'
 
 export default function Home() {
   const formData = useQRScoutState((state) => state.formData)
@@ -101,7 +102,6 @@ export default function Home() {
                 Reset
               </button>
               <div className="mb-4 flex flex-col justify-center rounded bg-white shadow-md dark:bg-gray-600">
-              <div className="m-2 flex flex-col justify-start bg-gray-500 p-2">
                 <div className="rounded-t pb-2 text-left font-bold text-white">
                   Theme
                 </div>
@@ -112,12 +112,6 @@ export default function Home() {
                   onChange={(v) => setTheme(v.target.value)}
                   value={theme}
                 >
-                  <option key={'system'} value={'system'}>
-                    System
-                  </option>
-                  <option key={'dark'} value={'dark'}>
-                    Dark
-                  </option>
                   <option key={'light'} value={'light'}>
                     Light
                   </option>
