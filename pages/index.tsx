ac80ca5f4bd3b7d9c58fd40eb48fa3c46897f2a3
aaquaa <113173827+aaquaa@users.noreply.github.com>
2023-12-31 14:58:46 +1100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,7 +101,7 @@ export default function Home() {
               >
                 Reset
               </button>
-              <div className="mb-4 flex flex-col justify-center rounded bg-white shadow-md dark:bg-gray-600">
+              <div className="m-2 flex flex-col justify-start bg-white p-2">
                 <div className="rounded-t pb-2 text-left font-bold text-white">
                   Theme
                 </div>
