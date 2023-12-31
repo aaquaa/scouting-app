d7630835d339017bb196fab5b09bad17fdcebe03
aaquaa <113173827+aaquaa@users.noreply.github.com>
2023-12-31 15:55:06 +1100

    Update CounterInput.tsx

--- components/inputs/CounterInput.tsx
+++ components/inputs/CounterInput.tsx
@@ -32,7 +32,7 @@ export default function CounterInput(data: CounterInputProps) {
       >
         -
       </button>
-      <h2 className="px-4 text-2xl">{data.value}</h2>
+      <h2 className="px-4 text-2xl dark:text-black">{data.value}</h2>
       <button
         className="focus:shadow-outline w-8 rounded bg-gray-500 text-2xl  text-white hover:bg-blue-700 focus:outline-none"
         type="button"
