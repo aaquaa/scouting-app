0dbd70e49d3c70fc5f3aef1a36ada31d46dfb42c
aaquaa <113173827+aaquaa@users.noreply.github.com>
2023-12-31 15:57:24 +1100

    Update SelectInput.tsx

--- components/inputs/SelectInput.tsx
+++ components/inputs/SelectInput.tsx
@@ -13,7 +13,7 @@ export default function SelectInput(data: SelectInputProps) {
   }
   return (
     <select
-      className="w-full rounded bg-white px-4 py-2"
+      className="w-full rounded bg-white px-4 py-2 dark:text-black"
       name={data.title}
       id={data.title}
       onChange={handleSelect}
